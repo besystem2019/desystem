cb12562613953393f6ba67f25cadaa790899095c
besystem2019 <58003535+besystem2019@users.noreply.github.com>
2019-11-21 22:20:48 -0600

    Update DESystem.java

--- src/desystem/DESystem.java
+++ src/desystem/DESystem.java
@@ -15,7 +15,7 @@ public class DESystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello there");
+        System.out.println("Hello there, general Kenobi!");
     }
     
 }
